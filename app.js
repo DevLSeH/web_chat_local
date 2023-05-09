00cb214ef96972989ae7410ae806050579fa345d
DevLSeH <dev.sehyeon@gmail.com>
2023-05-09 18:28:25 +0900

    change server side emit (all of message will send from server)/ add function for new client can see past message/ rooms are department now

--- app.js
+++ app.js
@@ -5,60 +5,76 @@ const { timeStamp, timeLog } = require("console");
 const SocketIO = require("socket.io");
 const io = SocketIO(server, {path:"/socket.io"});
 
+function messages(roomName){
+  this.roomName = roomName;
+  this.msgLog = [];
+}
+let mainCount = 0;
+const rooms = [];
 function createRoom(roomName){
-    const name = roomName;
-    const path = "/"+name;
-    const room= io.of(path);
+  const name = roomName;
+  const path = "/"+name;
+  const room= io.of(path);
+  
 
-    app.get(path, (req,res)=>{
-        res.sendFile(__dirname+"/room.html")
+  app.get(path, (req,res)=>{
+      res.sendFile(__dirname+"/room.html")
+  });
+  
+  const localCount = mainCount;
+  mainCount += 1;
+  rooms[localCount] = new messages(name);
+  const msgLog = rooms[localCount].msgLog;
+  
+  room.on("connection", (socket)=>{
+    const req = socket.request;
+    
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log("new Client Connected", ip, socket.id);
+
+    socket.emit("msg", msgLog);
+
+    socket.on("disconnect", ()=>{
+        console.log('Client is Disconnected', ip, socket.id);
+        clearInterval(socket.interval);
     });
 
-    room.on("connection", (socket)=>{
-        const req = socket.request;
-
-        const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-        console.log("new Client Connected", ip, socket.id);
-
-        socket.on("disconnect", ()=>{
-            console.log('Client is Disconnected', ip, socket.id);
-            clearInterval(socket.interval);
-        });
-
-        socket.on("error", (error)=>{
-            console.error(error.message);
-        });
-
-        socket.on("reply", (data)=>{
-            console.log(data);
-        });
-
-        socket.on("msg", (rawMsg)=>{
-            msg = JSON.parse(rawMsg);
-            console.log(`[${msg.time}]${msg.userId}: ${msg.msg}`);
-            socket.broadcast.emit("msg", rawMsg);
-        });
-        
-        socket.interval = setInterval(()=>{
-            socket.emit("news", "Hello Socket.IO");
-        }, 50000);
-        
+    socket.on("error", (error)=>{
+        console.error(error.message);
     });
+
+    socket.on("reply", (data)=>{
+        console.log(data);
+    });
+
+    socket.on("msg", (msg)=>{
+        console.log(`[${msg['time']}]${msg['userId']}: ${msg['msg']}`);
+        msgLog.push(msg);
+        socket.emit("msg",msgLog);
+        socket.broadcast.emit("msg", msgLog);
+        console.log(msgLog);
+    });
+    
+    socket.interval = setInterval(()=>{
+        socket.emit("news", "Hello Socket.IO");
+    }, 50000);
+      
+  });
 }
 
 const lobby = io.of("/");
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/lobby.html");
+  res.sendFile(__dirname+"/lobby.html");
 })
 lobby.on("connection", (socket)=>{
-    console.log("lobby connected");
-    
-    socket.on("create", (rawRoomInfo)=>{
-    const roomInfo = JSON.parse(rawRoomInfo);
-    const roomName = roomInfo.roomName;
-    console.log("create room :"+roomName);
-    createRoom(roomName);
-    socket.emit("created", roomName);
+  console.log("lobby connected");
+  
+  socket.on("create", (rawRoomInfo)=>{
+  const roomInfo = JSON.parse(rawRoomInfo);
+  const roomName = roomInfo.roomName;
+  console.log("create room :"+roomName);
+  createRoom(roomName);
+  socket.emit("created", roomName);
 })
 })
     
