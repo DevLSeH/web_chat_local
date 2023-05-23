5a0134b1994124629b8f288a9b9e99629e7ce706
DevLSeH <dev.sehyeon@gmail.com>
2023-05-24 08:54:52 +0900

    dd

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const app = require("express")();
 const server = app.listen(3000, () => {});
 const SocketIO = require("socket.io");
 const io = SocketIO(server, {path:"/socket.io"});
-console.log("hi");
 
 function messages(roomName){
   this.roomName = roomName;
